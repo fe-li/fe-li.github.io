1ed4d758c9c4a775989e1cba52206a5a2fddfb13
Kamil Feliszewski <kamil.feliszewski@outlook.com>
2021-09-26 10:11:17 +0200

    Delete subpages directory

--- subpages/sub.js
+++
@@ -1,9 +0,0 @@
-AOS.init();
-document.getElementById("aToHome").addEventListener("mouseover", (e) => {
-    e.target.style.transform = "translateX(75px)";
-    e.target.textContent = "fe-li.github.io";
-    e.target.addEventListener("mouseout", (el) => {
-        e.target.style.transform = "translateX(0px)";
-        e.target.textContent = "Kamil.";
-    })
-})
